769dda7907ffdabffa8109491a9c52e10dcd4278
MariN <donfog22@gmail.com>
2017-10-26 19:44:27 +0200

    errors, index i master view dodani

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+@extends('master')
+
+@section('title')
+
+@section('content')
+<div class="container">
+	<div class="col-md-10 col-md-offset-1">
+		<div class="panel panel-default">
+			<div class="panel heading">Recepti<br><a href="{{ url('/recipes/add') }}">
+				<i class="fa fa-btn fa-plus"></i>
+				dodaj novi recept
+			</a>
+			</div>
+			<div class="panel-body">
+				<ul class="list-group">
+				@foreach ($recipes as $recipe)
+				<li class="list-group-item">
+					<a href="recipes/view/{{ $recipe->id }}">{{ $recipe->name }}</a>
+				</li>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
